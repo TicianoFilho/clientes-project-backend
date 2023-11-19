451ddc6fa8e07ac209da82fb42b9df4e8fe3d4ec
TicianoFilho <ticianofilho@gmail.com>
2023-11-19 12:09:51 -0300

    create project

---
+++ src/main/java/br/com/study/vendasproject/VendasProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.vendasproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/study/vendasproject/VendasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.vendasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
