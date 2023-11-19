bf9d2abb02f51a8cab09c316bed9d044c2072d43
TicianoFilho <ticianofilho@gmail.com>
2023-11-19 12:29:08 -0300

    add lombok to the project

--- src/main/java/br/com/study/vendasproject/repository/Cliente.java
+++ src/main/java/br/com/study/vendasproject/repository/Cliente.java
@@ -1,11 +1,20 @@
 package br.com.study.vendasproject.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
 
     @Id
