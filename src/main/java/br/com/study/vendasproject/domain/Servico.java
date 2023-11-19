fb9bf0dc82946a25305a95af38c3cc576289a245
TicianoFilho <ticianofilho@gmail.com>
2023-11-19 17:18:25 -0300

    update

--- src/main/java/br/com/study/vendasproject/domain/Servico.java
+++ src/main/java/br/com/study/vendasproject/domain/Servico.java
@@ -1,11 +1,15 @@
 package br.com.study.vendasproject.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Servico {
 
