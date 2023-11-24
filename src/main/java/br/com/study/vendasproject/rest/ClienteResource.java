c3449336174d59b63d38494ae6c1f0dfaffb88b9
ticiano.filho <ticianofilho@gmail.com>
2023-11-24 17:28:31 -0300

    fix cors

--- src/main/java/br/com/study/vendasproject/rest/ClienteResource.java
+++ src/main/java/br/com/study/vendasproject/rest/ClienteResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
-@CrossOrigin("http://127.0.0.1:4200")
+@CrossOrigin(origins = {"http://127.0.0.1:4200", "http://localhost:4200"})
 public class ClienteResource {
 
     private final ClienteService clienteService;
