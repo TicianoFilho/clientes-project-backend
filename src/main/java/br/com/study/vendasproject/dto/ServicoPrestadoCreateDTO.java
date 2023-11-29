1d8c8481453fd3979b6376089c4c172c6b1821e4
ticiano.filho <ticianofilho@gmail.com>
2023-11-29 16:57:51 -0300

    fix validation

--- src/main/java/br/com/study/vendasproject/dto/ServicoPrestadoCreateDTO.java
+++ src/main/java/br/com/study/vendasproject/dto/ServicoPrestadoCreateDTO.java
@@ -5,16 +5,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoCreateDTO {
 
+    @NotEmpty(message = "{servico.prestado.field.descricao.required}")
     private String descricao;
 
     @JsonProperty("clienteId")
+    @NotNull(message = "{servico.prestado.field.cliente.required}")
     private Long clienteCode;
 
+    @NotNull(message = "{servico.prestado.field.valor.required}")
     private BigDecimal valor;
 }
