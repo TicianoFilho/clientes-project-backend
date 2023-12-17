12860f14be70ed536e55daf4959d3064c325b89b
TicianoFilho <ticianofilho@gmail.com>
2023-12-17 01:00:18 -0300

    configuring routes

--- src/main/java/br/com/study/vendasproject/config/security/ResourceServerConfig.java
+++ src/main/java/br/com/study/vendasproject/config/security/ResourceServerConfig.java
@@ -1,4 +1,24 @@
 package br.com.study.vendasproject.config.security;
 
-public class ResourceServerConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${api.base.path}")
+    private String apiBasePath;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // Configurações relativas aos Resources
+        http
+                .authorizeRequests()
+                .antMatchers(apiBasePath + "/api/v1/usuarios").permitAll()
+                .antMatchers(apiBasePath + "/api/v1/clientes/**", apiBasePath + "/api/v1/servicos-prestados/**").authenticated()
+                .anyRequest().denyAll();
+    }
 }
--- src/main/java/br/com/study/vendasproject/config/security/SecurityConfig.java
+++ src/main/java/br/com/study/vendasproject/config/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Configurações relativas à web
         http.
                 csrf().disable()
                 .cors()
